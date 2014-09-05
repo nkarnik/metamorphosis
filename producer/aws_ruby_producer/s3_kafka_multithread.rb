bcc774850ab7ef3377bda51a6461a8616d0c3f25
Nikhil Karnik <nikhil@zillabyte.com>
2014-09-05 15:57:38 -0700

    added multithreaded production

---
+++ producer/aws_ruby_producer/s3_kafka_multithread.rb
@@ -0,0 +1,64 @@
+require 'thread'
+require "aws"
+require "poseidon"
+
+work_q = Queue.new
+shards = ARGV.first
+
+AWS.config(
+          :access_key_id    => 'AKIAJWZ2I3PMFF5O6PFA',
+          :secret_access_key => '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'
+          )
+
+s3 = AWS::S3.new
+
+producer = Poseidon::Producer.new(["10.167.177.28:9092", "10.184.165.95:9092" ], "st1", :type => :sync)
+
+`rm -r /tmp/kfk2`
+`mkdir /tmp/kfk2`
+
+bucket = s3.buckets["fatty.zillabyte.com"]
+tree = bucket.as_tree(:prefix => 'data/homepages/2014/0822')
+directories = tree.children.select(&:branch?).collect(&:prefix)
+
+shards = ARGV.first.to_i
+
+files = bucket.as_tree(:prefix => directories.first).children.select(&:leaf?).collect(&:key)
+files.each do |file|
+  break if shards == 0
+  work_q << file
+  puts file
+  shards -= 1
+end
+
+puts "/n /n"
+
+start = Time.now
+puts start
+workers = (0...10).map do
+  Thread.new do
+    begin
+      while f = work_q.pop(true)
+          puts f
+          path = "/tmp/kfk2/" + f.split("/").last(2).join("_")
+          File.open(path, 'wb') do |file|
+            bucket.objects[f].read do |chunk|
+              file.write(chunk)
+            end
+          end
+          s3file = open(path)
+
+          gz = Zlib::GzipReader.new(s3file)
+          gz.each_line do |line|
+            producer.send_messages([Poseidon::MessageToSend.new("nk8", line)])
+          end
+          puts Time.now - start
+      end
+    rescue ThreadError
+    end
+  end
+end
+
+workers.map(&:join); "ok"
+endt = Time.now
+puts endt - start

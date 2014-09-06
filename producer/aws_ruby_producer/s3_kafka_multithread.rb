9bbf1a925571f576f84f20cb8a4c3287bf04481c
Nikhil Karnik <nikhil@zillabyte.com>
2014-09-05 17:14:35 -0700

    finalized s3_kafka_multithread.rb for homepages...Sashi: this takes 1 command line arg (number of shards)

--- producer/aws_ruby_producer/s3_kafka_multithread.rb
+++ producer/aws_ruby_producer/s3_kafka_multithread.rb
@@ -3,7 +3,8 @@ require "aws"
 require "poseidon"
 
 work_q = Queue.new
-shards = ARGV.first
+shards = ARGV.first.to_i
+`touch log_kafka`
 
 AWS.config(
           :access_key_id    => 'AKIAJWZ2I3PMFF5O6PFA',
@@ -14,51 +15,61 @@ s3 = AWS::S3.new
 
 producer = Poseidon::Producer.new(["10.167.177.28:9092", "10.184.165.95:9092" ], "st1", :type => :sync)
 
-`rm -r /tmp/kfk2`
-`mkdir /tmp/kfk2`
 
 bucket = s3.buckets["fatty.zillabyte.com"]
-tree = bucket.as_tree(:prefix => 'data/homepages/2014/0822')
+tree = bucket.as_tree(:prefix => 'data/homepages/2014/0620')
 directories = tree.children.select(&:branch?).collect(&:prefix)
 
-shards = ARGV.first.to_i
+st = Time.now
 
-files = bucket.as_tree(:prefix => directories.first).children.select(&:leaf?).collect(&:key)
-files.each do |file|
+directories.each do |dir|
+  puts shards
   break if shards == 0
-  work_q << file
-  puts file
-  shards -= 1
-end
 
-puts "/n /n"
+  `rm -r /tmp/kfk2`
+  `mkdir /tmp/kfk2`
+  files = bucket.as_tree(:prefix => dir).children.select(&:leaf?).collect(&:key)
+  files.each do |file|
+    break if shards == 0
+    work_q << file
+    puts file
+    shards -= 1
+  end
 
-start = Time.now
-puts start
-workers = (0...10).map do
-  Thread.new do
-    begin
-      while f = work_q.pop(true)
-          puts f
-          path = "/tmp/kfk2/" + f.split("/").last(2).join("_")
-          File.open(path, 'wb') do |file|
-            bucket.objects[f].read do |chunk|
-              file.write(chunk)
+  puts "/n /n"
+
+  start = Time.now
+  puts start
+  workers = (0...15).map do
+    Thread.new do
+      begin
+        while f = work_q.pop(true)
+            puts f
+            path = "/tmp/kfk2/" + f.split("/").last(2).join("_")
+            File.open(path, 'wb') do |file|
+              bucket.objects[f].read do |chunk|
+                file.write(chunk)
+              end
             end
-          end
-          s3file = open(path)
+            s3file = open(path)
 
-          gz = Zlib::GzipReader.new(s3file)
-          gz.each_line do |line|
-            producer.send_messages([Poseidon::MessageToSend.new("nk8", line)])
-          end
-          puts Time.now - start
+            gz = Zlib::GzipReader.new(s3file)
+            gz.each_line do |line|
+              producer.send_messages([Poseidon::MessageToSend.new("nk8", line)])
+            end
+            diff = Time.now - st
+            info = diff.to_s + "   " + path + "   shards left:   " + shards.to_s
+            puts info
+            `echo #{info} >> log_kafka`
+        end
+      rescue ThreadError
       end
-    rescue ThreadError
     end
   end
-end
 
-workers.map(&:join); "ok"
+  workers.map(&:join); "ok"
+  endt = Time.now
+  puts endt - start
+end
 endt = Time.now
-puts endt - start
+puts endt - st

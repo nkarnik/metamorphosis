bf13a796bb5cb426f4560a304833ec3c59484b3e
Sashikanth <d.sashikanth@gmail.com>
2014-09-10 02:34:19 -0700

    Down to 35seconds for 100 shards. 4 threads, one producer per partition

--- producer/aws_ruby_producer/debugging_producer_kafka.rb
+++ producer/aws_ruby_producer/debugging_producer_kafka.rb
@@ -27,6 +27,11 @@ opt_parser = OptionParser.new do |opt|
   opt.on("--topic TOPIC", String, "Set the AMI to use") do |topic|
     $options[:topic] = topic
   end
+
+  opt.on("--partition", String, "Produce to a specific partition") do |topic|
+    $options[:partition] = partition
+  end
+
 end
 
 opt_parser.parse!
@@ -35,16 +40,16 @@ opt_parser.parse!
 work_q = Queue.new
 
 #first arg = # of shards to pull
-shards = $options[:num_shards] || 5
+num_shards = $options[:num_shards].to_i || 5
 
 #second arg = environment (test/prod)
-e = $options[:env] || "prod"
+env = $options[:env] || "prod"
 
 #third arg = topic name
 topic = $options[:topic] || "s4"
 
-num_threads = $options[:threads] || "5"
-puts $options
+num_threads = $options[:threads].to_i || "5"
+puts "Options: #{$options}"
 
 LOGFILE = "kafka_producer.log"
 `touch #{LOGFILE}`
@@ -67,45 +72,45 @@ s3 = AWS::S3.new
 
 def find_broker(env, attrib = 'fqdn')
   results = `knife search node "role:kafka_broker AND chef_environment:#{env}" -F json -a fqdn  -c ~/zb1/infrastructure/chef/.chef/knife.rb`
-  hash = JSON.parse(results)
-  hash["rows"].map do |rec|
-    rec.values.first[attrib]
-  end
-  puts hash
-  return hash
+  return JSON.parse(results)["rows"].map{ |a| a.map{|k,v| v["fqdn"]}  }.map{|v| "#{v[0]}:9092"}
 end
-
-a = find_broker(e)
-ips = []
-
-a["rows"]. each do |row|
-  row.each do |k, v|
-    ips << v["fqdn"]
-  end
-end
-
 #parse role => ip mapping to get correct format for kafka broker
-fqdns = []
-ips.each do |ip|
-  s = ip.split(".")
-  t = s[0].split("-")
-  fqdn = t[1, t.length].join(".") + ":9092"
-  # puts fqdn
-  fqdns << fqdn
-end
 
-if e == "local"
+
+if env == "local"
   fqdns = ["localhost:9092"]
+else
+  fqdns = find_broker(env)
 end
 
 puts "fqdns: #{fqdns}"
 
 #create producer with list of domains
 
-
 bucket = s3.buckets["fatty.zillabyte.com"]
 tree = bucket.as_tree(:prefix => 'data/homepages/2014/0620')
 directories = tree.children.select(&:branch?).collect(&:prefix)
+
+
+`rm -r /tmp/kfk2`
+`mkdir /tmp/kfk2`
+
+shards = 1
+directories.each do |dir|
+  # puts "shards: #{shards}"
+  # puts "dir: #{dir}"
+  break if shards == num_shards
+  files = bucket.as_tree(:prefix => dir).children.select(&:leaf?).collect(&:key)
+  files.each do |file|
+    work_q << file
+    # log "Appended shard [#{shards}/#{num_shards}]: #{file}"
+    break if shards == num_shards
+    shards += 1
+  end
+end
+
+log "\n#{shards.size} shards appended\n"
+
 
 st = Time.now
 
@@ -116,81 +121,68 @@ cluster_metadata.update(broker_pool.fetch_metadata([topic]))
 
 metadata = cluster_metadata.topic_metadata[topic]
 num_partitions = metadata.partition_count
-leaders_per_partition = []
+@leaders_per_partition = []
 metadata.partition_count.times do |part_num|
-  leaders_per_partition << cluster_metadata.lead_broker_for_partition(topic, 0).host
+  @leaders_per_partition << cluster_metadata.lead_broker_for_partition(topic, 0).host
 end
 
-num_threads = num_partitions
+log "Leaders per partition: #{@leaders_per_partition}"
 
-log "done"
+# binding.pry
 
-binding.pry
+start = Time.now
+puts "Start time: #{start}"
 
-directories.each do |dir|
-  puts "shards: #{shards}"
-  puts "dir: #{dir}"
-  break if shards == 0
+workers = []
+num_partitions.times do |partition_num|
+  
+  t = Thread.new do
+    begin
+      single_partitioner = Proc.new { |key, partition_count| partition_num  } # Will always right to a single partition
+      producer_fqdn = @leaders_per_partition[partition_num]
+      producer = Poseidon::Producer.new([producer_fqdn], "producer_#{partition_num}", :type => :sync, :partitioner => single_partitioner)
+      puts "producer: #{producer} for node: #{producer_fqdn}"
+      while f = work_q.pop(true)
+        per_shard_time = Time.now
+        path = "/tmp/kfk2/" + f.split("/").last(2).join("_")
+        File.open(path, 'wb') do |file|
+          bucket.objects[f].read do |chunk|
+            begin
+              file.write(chunk)
+            rescue
+              log "s3 error"
 
-  `rm -r /tmp/kfk2`
-  `mkdir /tmp/kfk2`
-  files = bucket.as_tree(:prefix => dir).children.select(&:leaf?).collect(&:key)
-  files.each do |file|
-    break if shards == 0
-    work_q << file
-    puts file
-    shards -= 1
-  end
-
-  puts "\n \n"
-
-  start = Time.now
-  puts start
-  workers = []
-  num_threads.times do |thread_num|
-    t = Thread.new do
-      begin
-        single_partitioner = Proc.new { |key, partition_count| thread_num  } # Will always right to a single partition
-
-	producer = Poseidon::Producer.new([fqdns[thread_num % fqdns.size]], "producer_#{thread_num}", :type => :sync)
-	puts "producer: #{producer} for node: #{fqdns[thread_num % fqdns.size]}"
-        while f = work_q.pop(true)
-
-            puts f
-            path = "/tmp/kfk2/" + f.split("/").last(2).join("_")
-            File.open(path, 'wb') do |file|
-              bucket.objects[f].read do |chunk|
-                begin
-                  file.write(chunk)
-                rescue
-                  log "s3 error"
-
-                end
-              end
             end
-            s3file = open(path)
-
-            gz = Zlib::GzipReader.new(s3file)
-            gz.each_line do |line|
-              begin
-                producer.send_messages([Poseidon::MessageToSend.new(topic, line)])
-              rescue Exception => e
-                log "empty message error"
-              end
-            end
-            diff = Time.now - st
-            log(diff.to_s + "   " + path + "   shards left: " + shards.to_s + " current time: " + Time.now.to_s)
-            
+          end
         end
-      rescue ThreadError
-      end
-    end
-    workers << t
-  end
+        s3file = open(path)
 
-  workers.map(&:join); "ok"
-  endt = Time.now
-  puts endt - start
+        gz = Zlib::GzipReader.new(s3file)
+        msgs = []
+        gz.each_line do |line|
+          begin
+            msgs << Poseidon::MessageToSend.new(topic, line)
+          rescue Exception => e
+            log "empty message error"
+          end
+        end 
+        producer.send_messages(msgs)
+        log( "#{Time.now.to_s}:: #{path} shards completed:  #{work_q.size}. per shard time: #{Time.now - per_shard_time}. Total time since start: #{Time.now - st}")
+
+            
+      end
+    rescue ThreadError
+    end
+  end
+  workers << t
 end
+
+workers.map(&:join); 
+
+puts "Done with the threads"
+
+endt = Time.now
+puts endt - start
+
 endt = Time.now
 puts endt - st

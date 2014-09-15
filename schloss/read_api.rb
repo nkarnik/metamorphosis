be104aed50064c59dad1d6521fbfd59014582b6d
Ubuntu <ubuntu@ip-10-81-164-79.ec2.internal>
2014-09-15 16:44:02 +0000

    functional service to read special topic dedicated to API and write to broker topics

--- schloss/read_api.rb
+++ schloss/read_api.rb
@@ -14,6 +14,12 @@ def log(msg)
   @lf.flush
 end
 
+AWS.config(
+          :access_key_id    => 'AKIAJWZ2I3PMFF5O6PFA',
+          :secret_access_key => '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'
+          )
+
+s3 = AWS::S3.new
 
 $options = {}
 
@@ -61,15 +67,36 @@ fqdns.each do |host|
   con += 1
 end
 
+local_manifest = "manifest"
+
 loop do
   consumers.each do |consumer|
-    puts consumer, consumer.host
+    #puts consumer, consumer.host
     begin
       messages = consumer.fetch({:max_bytes => 100000})
-      puts messages
+      #puts messages
       messages.each do |m|
-        puts m.value
+        message = m.value.split()
         log m.value
+        bucket_name = message[0]
+        manifest_path = message[1]
+        bucket = s3.buckets[bucket_name]
+        log "Downloading Manifest"
+
+        File.open(local_manifest, 'wb') do |file|
+          bucket.objects[manifest_path].read do |chunk|
+            begin
+              file.write(chunk)
+            rescue
+              log "s3 error for path: #{f}"
+            end
+          end
+        end
+
+        File.open(local_manifest).each do |line|
+          log line
+        end 
+
       end
     rescue
       puts "error"

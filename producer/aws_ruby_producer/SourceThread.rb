683e1c5adb0ef05b6deff6a7df7c684733abbec2
Nikhil Karnik <nikhil@zillabyte.com>
2014-09-19 00:44:17 -0700

    more modular classes for producer threads reading from arbitrary sources

---
+++ producer/aws_ruby_producer/SourceThread.rb
@@ -0,0 +1,162 @@
+require 'thread'
+require 'aws-sdk'
+require 'poseidon'
+require 'json'
+require './TopicProducerConfig'
+
+class ProducerThread
+  attr_reader :thread, :sourcetype, :sourceconfig, :source, :topic
+  def initialize(LOGFILE)
+    @sourcetype = None
+    @sourceconfig = None
+    @source = None
+    @topic = None
+    @thread = None
+    @logfile = LOGFILE
+
+  end
+
+  def log(msg)
+    if @lf.nil?
+      @lf = File.open(@logfile, 'a')
+    end
+    puts "#{Time.now}: #{msg}\n"
+    @lf.write "#{Time.now}: #{msg}\n"
+    @lf.flush
+  end
+
+  def createSource()
+    if @sourcetype == "s3"
+      source = S3Source.new(@sourceconfig)
+      return source
+    end
+
+  def start(work_q, topic_producer_hash)
+    @thread = Thread.new do
+      num_msgs_per_thread = 0
+      begin
+        # We want producers exclusive to threads.
+        # producers_for_thread = @producers.select.with_index{|_,i| i % num_threads == thread_num}
+        # log "producer: #{producer} for node: #{producer_fqdn}"
+        log "Number of shards left to push into kafka: " + work_q.size.to_s
+
+        while s = work_q.pop(true)
+          @sourcetype = s["sourcetype"]
+          @sourceconfig = s["sourceconfig"]
+          @source = createSource()
+          @topic = s["topic"]
+
+          topicproducer = topic_producer_hash[@topic]
+          partitions_for_thread = topicproducer.partitions_on_localhost.select.with_index{|_,i| i % num_threads == thread_num}
+
+          #Get path of file where data is written to locally, and write data there...
+          path = @source.path
+          @source.getData()
+
+
+          #Select partition by sampling
+          partition = partitions_for_thread.sample
+          producer = topicproducer.get_producer_for_partition(partition)
+
+          datafile = open(path)
+          # log "Downloaded: #{path} in #{Time.now - per_shard_time}"
+          gz = Zlib::GzipReader.new(datafile)
+          msgs = []
+          sent_messages = 0
+          batch_size = 1000
+          gz.each_line do |line|
+            begin
+              msgs << Poseidon::MessageToSend.new(@topic, line)
+              # producer.send_messages([Poseidon::MessageToSend.new(topic, line)])
+              # binding.pry
+            rescue Exception => e
+              log "Error: #{e.message}"
+            end
+          end
+          if producer.send_messages(msgs)
+            sent_messages += msgs.size
+            num_msgs_per_thread += sent_messages
+          else
+            log "Message failed: Batch size #{msgs.size} Sent from this shard: #{sent_messages}"
+          end
+
+          # binding.pry
+          # Send each shard to an arbitrary partition
+          # producer.send_messages(msgs)
+          File.delete path
+          shard_num += 1
+          log( "#T: #{thread_num} P#: #{partition}\t #{(Time.now - per_shard_time).round(2)}s \t#{num_msgs_per_thread} msgs. #{path.gsub("/tmp/", "")} \tshard: #{} \tRem:#{work_q.size}.  since: #{((Time.now - start_time) / 60.0).round(2)} min")
+        end
+      rescue SystemExit, Interrupt, Exception => te
+        log "Thread Completed: #{te.message}.\n\n\tTotal Messages for this thread: #{num_msgs_per_thread}\n\n"
+        puts te.backtrace unless te.message.include?("queue empty")
+        Thread.exit
+      end
+    end
+  end
+end
+
+class KafkaSource
+
+  def initialize(sourceconfig, LOGFILE)
+    #validate input of
+
+    @logfile = LOGFILE
+
+  end
+
+  def log(msg)
+    if @lf.nil?
+      @lf = File.open(LOGFILE, 'a')
+    end
+    puts "#{Time.now}: #{msg}\n"
+    @lf.write "#{Time.now}: #{msg}\n"
+    @lf.flush
+  end
+
+  def getData()
+    #do some generic stuff
+  end
+
+end
+
+
+class S3Source < KafkaSource
+  attr_reader :path
+  def initialize(sourceconfig, LOGFILE)
+    AWS.config(
+              :access_key_id    => 'AKIAJWZ2I3PMFF5O6PFA',
+              :secret_access_key => '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'
+              )
+
+    @logfile = LOGFILE
+    @_s3 = AWS::S3.new
+    @_bucket = @_s3.buckets[sourceconfig["bucket"]]
+
+    f = sourceconfig["shard"].chomp.gsub("s3://#{sourceconfig["bucket"]}/", "")
+    #log "Preparing shard"
+    per_shard_time = Time.now
+    @path = "/tmp/" + f.split("/").last(2).join("_")
+
+  end
+
+  def getData()
+
+    begin
+
+      File.open(@path, 'wb') do |file|
+        @_bucket.objects[f].read do |chunk|
+          begin
+            file.write(chunk)
+          rescue
+            log "s3 error for path: #{f}"
+          end
+        end
+      end
+    rescue Exception => e
+      log "Failed shard. Moving on: #{f}"
+      next
+    end
+
+  end
+end
--- producer/aws_ruby_producer/generic_shard_producer.rb
+++ producer/aws_ruby_producer/generic_shard_producer.rb
@@ -5,6 +5,7 @@ require 'poseidon'
 require 'optparse'
 require 'pry'
 require './TopicProducerConfig'
+require './SourceThread'
 
 def log(msg)
   if @lf.nil?
@@ -20,7 +21,6 @@ AWS.config(
           :secret_access_key => 'F9rmZ36zlk2rNNRunsbYQh53+OF6rPdzy6HtI6bf'
           )
 
-s3 = AWS::S3.new
 
 host = `hostname`.chomp
 queue_name =  "#{host}.ec2.internaltest1"
@@ -60,7 +60,7 @@ num_threads = $options[:threads].to_i || "1"
 num_shards = $options[:num_shards].to_i || "10"
 $bucket_name = "fatty.zillabyte.com"
 
-work_q = Queue.new
+$work_q = Queue.new
 
 LOGFILE = "queue_consumer.log"
 `touch #{LOGFILE}`
@@ -102,23 +102,23 @@ end
 def read_from_queue(cons, worker_q)
   loop do
     cons.each do |consumer|
-      #Fetch messages from dedicated worker queue, push message to work_q 
+      #Fetch messages from dedicated worker queue, push message to work_q
       begin
-        messages = consumer.fetch({:max_bytes => 100000}) 
+        messages = consumer.fetch({:max_bytes => 100000})
         messages.each do |m|
           message = m.value
           #log message
           message = JSON.parse(message)
-  
-          topic = message["topic"]          
+
+          topic = message["topic"]
           worker_q << message
           #log worker_q.size
-          
-          # build TopicProducer configuration object for topic 
+
+          # build TopicProducer configuration object for topic
           if not $topic_producer_hash.has_key?(topic)
             log "Creating new producer for #{topic}"
-            topicproducer = TopicProducerConfig.new($broker_pool, topic) 
-            $topic_producer_hash[topic] = topicproducer 
+            topicproducer = TopicProducerConfig.new($broker_pool, topic)
+            $topic_producer_hash[topic] = topicproducer
             a = topicproducer.partitions_on_localhost
             #log "Finished creating producer for #{topic}"
           end
@@ -140,89 +140,14 @@ sleep 5
 start_time = Time.now
 puts "Start time: #{start_time}"
 
+#Spin up selected number of threads, generate sources based on configs
 workers = []
 num_threads.times do |thread_num|
 
-  t = Thread.new do
-    num_msgs_per_thread = 0
-    begin
-      # We want producers exclusive to threads.
-      # producers_for_thread = @producers.select.with_index{|_,i| i % num_threads == thread_num}
-      # log "producer: #{producer} for node: #{producer_fqdn}"
-      log "Number of shards left to push into kafka: " + work_q.size.to_s
- 
-      while s = work_q.pop(true)
-        _s3 = AWS::S3.new
-        # Bucket per thread
-        _bucket = _s3.buckets[s["bucket"]]
-        _topic = s["topic"]
+  t = SourceThread::ProducerThread.new
+  t.start($work_q, $topic_producer_hash)
+  workers << t.thread
 
-        topicproducer = $topic_producer_hash[_topic]      
-        partitions_for_thread = topicproducer.partitions_on_localhost.select.with_index{|_,i| i % num_threads == thread_num}
-
-        shard_num = 0
-
-        f = s["shard"].chomp.gsub("s3://fatty.zillabyte.com/", "")
-        topic = s["topic"]
-        log "Preparing shard: " + f
-        
-        per_shard_time = Time.now
-        path = "/tmp/" + f.split("/").last(2).join("_")
-
-        begin
-
-          File.open(path, 'wb') do |file|
-            _bucket.objects[f].read do |chunk|
-              begin
-                file.write(chunk)
-              rescue
-                log "s3 error for path: #{f}"
-              end
-            end
-          end
-        rescue Exception => e
-          log "Failed shard. Moving on: #{f}"
-          next
-        end
-        partition = partitions_for_thread.sample
-        producer = topicproducer.get_producer_for_partition(partition)
-
-        s3file = open(path)
-        # log "Downloaded: #{path} in #{Time.now - per_shard_time}"
-        gz = Zlib::GzipReader.new(s3file)
-        msgs = []
-        sent_messages = 0
-        batch_size = 1000
-        gz.each_line do |line|
-          begin
-            msgs << Poseidon::MessageToSend.new(topic, line)
-            # producer.send_messages([Poseidon::MessageToSend.new(topic, line)])
-            # binding.pry
-          rescue Exception => e
-            log "Error: #{e.message}"
-          end
-        end
-        if producer.send_messages(msgs)
-          sent_messages += msgs.size
-          num_msgs_per_thread += sent_messages
-        else
-          log "Message failed: Batch size #{msgs.size} Sent from this shard: #{sent_messages}"
-        end
-
-        # binding.pry
-        # Send each shard to an arbitrary partition
-        # producer.send_messages(msgs)
-        File.delete path
-        shard_num += 1
-        log( "#T: #{thread_num} P#: #{partition}\t #{(Time.now - per_shard_time).round(2)}s \t#{num_msgs_per_thread} msgs. #{path.gsub("/tmp/", "")} \tshard: #{} \tRem:#{work_q.size}.  since: #{((Time.now - start_time) / 60.0).round(2)} min")
-      end
-    rescue SystemExit, Interrupt, Exception => te
-      log "Thread Completed: #{te.message}.\n\n\tTotal Messages for this thread: #{num_msgs_per_thread}\n\n"
-      puts te.backtrace unless te.message.include?("queue empty")
-      Thread.exit
-    end
-  end
-  workers << t
 end
 
 workers.map(&:join);

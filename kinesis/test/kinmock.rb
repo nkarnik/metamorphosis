ca9385469030a1c42332eb910ea6cf043d1ffe9f
Nikhil Karnik <nikhil@zillabyte.com>
2014-09-24 09:48:51 -0700

    figured out how to buffer messages properly

--- kinesis/test/kinmock.rb
+++ kinesis/test/kinmock.rb
@@ -80,11 +80,31 @@ loop do
 end
 
 
+#loop do
+#  sharditers.each do |si|
+#    log si[:sqn]
+#    log si[:sid].class
+#    sharditer = kclient.get_shard_iterator(:stream_name => "TestStream", :shard_id => si[:sid], :shard_iterator_type => "LATEST") 
+#    records = kclient.get_records(:shard_iterator => sharditer.shard_iterator)
+#    log records.records.length
+#    records.records.each do |record|
+#      log "#{record.sequence_number} #{record.data}"
+#      sqnum = record.sequence_number
+#      si[:sqn] = sqnum.to_s
+#      #br = true
+#      log "#{si[:sqn]} #{si[:sid]}"
+#    end
+#    si = records.next_shard_iterator
+#    log "tried records"
+#  end
+#end
+
+
 loop do
   sharditers.each do |si|
     log si[:sqn]
     log si[:sid].class
-    sharditer = kclient.get_shard_iterator(:stream_name => "TestStream", :shard_id => si[:sid], :shard_iterator_type => "AT_SEQUENCE_NUMBER", :starting_sequence_number => si[:sqn]) 
+    sharditer = kclient.get_shard_iterator(:stream_name => "TestStream", :shard_id => si[:sid], :shard_iterator_type => "AFTER_SEQUENCE_NUMBER", :starting_sequence_number => si[:sqn]) 
     records = kclient.get_records(:shard_iterator => sharditer.shard_iterator)
     log records.records.length
     records.records.each do |record|

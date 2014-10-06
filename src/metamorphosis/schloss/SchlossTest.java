6ca3a5c59c8836608afb1f2cd538b93f31c792e0
Sashikanth <d.sashikanth@gmail.com>
2014-10-06 13:50:23 -0700

    pom.xml bs

---
+++ src/metamorphosis/schloss/SchlossTest.java
@@ -0,0 +1,16 @@
+package metamorphosis.schloss;
+
+import kafka.server.KafkaServer;
+
+import org.junit.Before;
+
+public class SchlossTest {
+
+
+  @Before
+  public void setup(){
+    KafkaServer s;
+  }
+
+  
+}

649148e484322884d09f411b80f0c944161db797
Sashikanth <d.sashikanth@gmail.com>
2014-12-05 15:39:37 -0800

    not gaming the tests

--- test/metamorphosis/workers/sinks/test/WorkerElasticsearchSinkTest.java
+++ test/metamorphosis/workers/sinks/test/WorkerElasticsearchSinkTest.java
@@ -149,6 +149,6 @@ public class WorkerElasticsearchSinkTest {
     totalSunk = response.getCount();
     client.close();
     _log.info("Final number of messages in elasticsearch: " + totalSunk);
-    assertEquals(9881, totalSunk);
+    assertEquals(10000, totalSunk);
   }
 }
